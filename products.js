bdbdac15c22f7305ba841cb92377d0ff95b2d87e
Cevi Taufik <77260222+cevitaufik@users.noreply.github.com>
2021-07-11 22:29:14 +0700

    initial commit

---
+++ products.js
@@ -0,0 +1,77 @@
+{ "products" : [
+    {
+      "id" : "a",
+      "nama" : "Original kaos Homicide Godzkilla Necronometry",
+      "harga" : "Rp. 275.000",
+      "kualitas": "original",      
+      "kondisi" : "Bekas",
+      "ukuran" : "M",
+      "stock" : 1,
+      "merek" : "Grimloc",
+      "bahan" : "Katun",
+      "cover": "products/hmcd1-1.jpg",
+      "gbr1" : "products/hmcd1-2.jpg",
+      "gbr2" : "products/hmcd1-3.jpg",
+      "shopee" : "https://shopee.co.id/Original-kaos-Homicide-Godzkilla-Necronometry-i.216352831.9668656195"   
+    },
+    {
+      "id" : "b",
+      "nama" : "Original kaos Homicide Barisan Nisan",
+      "harga" : "Rp. 275.000",
+      "kualitas": "original",      
+      "kondisi" : "Bekas",
+      "ukuran" : "M",
+      "stock" : 1,
+      "merek" : "Grimloc",
+      "bahan" : "Katun",
+      "cover": "products/hmcd2-1.jpg",
+      "gbr1" : "products/hmcd2-2.jpg",
+      "gbr2" : "products/hmcd2-3.jpg",
+      "shopee" : "https://shopee.co.id/Original-Kaos-Homicide-i.216352831.2969666258"
+    },
+    {
+      "id" : "c",
+      "nama" : "BARU Original Kaos Homicide 10th year Illsurrekshun",
+      "harga" : "Rp. 350.000",
+      "kualitas": "original",      
+      "kondisi" : "Baru",
+      "ukuran" : "M",
+      "stock" : 1,
+      "merek" : "Uprock 83",
+      "bahan" : "Katun",
+      "cover": "products/hmcd3-1.jpg",
+      "gbr1" : "products/hmcd3-2.jpg",
+      "gbr2" : "products/hmcd3-3.jpg",
+      "shopee" : "https://shopee.co.id/BARU-Original-Kaos-Homicide-10th-year-Illsurrekshun-i.216352831.9768666849"
+    },
+    {
+      "id" : "d",
+      "nama" : "ORI TANDA TANGAN Catatan Najwa - Najwa Shihab",
+      "harga" : "Rp. 150.000",
+      "penulis" : "Najwa Shihab",
+      "kualitas": "original + tandatangan",
+      "kondisi" : "Bekas",
+      "stock" : 1,
+      "bahan": "bookpaper",
+      "cover" : "products/catna1-1.jpg",
+      "gbr1" : "products/catna1-2.jpg",
+      "gbr2" : "products/catna1-3.jpg",
+      "gbr3" : "products/catna1-4.jpg",
+      "shopee" : "https://shopee.co.id/ORI-TANDA-TANGAN-Catatan-Najwa-Najwa-Shihab-i.216352831.11612484262"
+    },
+    {
+      "id" : "e",
+      "nama" : "TEROR ORDE BARU",
+      "harga" : "Rp. 85.000",
+      "penulis" : "Julie Southwood - Patrick Planagan",
+      "kualitas": "original",
+      "kondisi" : "Bekas",
+      "stock" : 1,
+      "bahan": "bookpaper",
+      "cover": "products/teor1-1.jpg",
+      "gbr1" : "products/teor1-2.jpg",
+      "gbr2" : "products/teor1-3.jpg",
+      "shopee" : "https://shopee.co.id/buku-TEROR-ORDE-BARU-(PRELOVED)-i.216352831.10110530261"
+    }
+  ]
+}
